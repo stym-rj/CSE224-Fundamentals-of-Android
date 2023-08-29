8ea7320bbc15853d0d0d9a6d5123a37d8c597e8f
satyam raj <satyam3279raj@gmail.com>
2023-08-29 19:55:48 +0530

    added final project

---
+++ 1 Create the User Interface in Android Studio/week5/Little Lemon Project/app/src/main/java/com/example/littlelemon/ui/theme/Theme.kt
@@ -0,0 +1,21 @@
+package com.example.littlelemon.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+
+private val LightColorPalette = lightColors(
+    primary = LittleLemonColor.yellow,
+    secondary = LittleLemonColor.pink
+)
+
+@Composable
+fun LittleLemonTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
